f8e1e93c1302de5152fcd2a5724d642d2041279a
Alex <Alex_Neha_92@mail.ru>
2020-04-20 15:06:48 +0300

    Изменил настройки почты

--- superlists/settings.py
+++ superlists/settings.py
@@ -127,11 +127,11 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.abspath(os.path.join(BASE_DIR, '../static'))
 
-# EMAIL_HOST = 'smtp.yandex.ru'
-# EMAIL_HOST_USER = 'AlexUndead1992'
-# EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD')
-# EMAIL_PORT = 465
-# EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.yandex.ru'
+EMAIL_HOST_USER = 'AlexUndead1992@yandex.ru'
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD')
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
 
-EMAIL_HOST = 'localhost'
-EMAIL_PORT = 1025
+#EMAIL_HOST = 'localhost'
+#EMAIL_PORT = 1025
